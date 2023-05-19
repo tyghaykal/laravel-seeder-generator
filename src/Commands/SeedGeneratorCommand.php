d767ca087450c94c40c17e9d19602abfe93bd7ce
Andre Haykal Rachman <andrecsd9@gmail.com>
2023-05-20 02:50:57 +0700

    progress

--- src/Commands/SeedGeneratorCommand.php
+++ src/Commands/SeedGeneratorCommand.php
@@ -9,30 +9,34 @@ use TYGHaykal\LaravelSeedGenerator\Helpers\StringHelper;
 
 class SeedGeneratorCommand extends Command
 {
-    protected $signature = "seed:generate {model} {--ids= : The ids to be seeded} {--ignore-ids= : The ids to be ignored} {--fields= : The fields to be seeded} {--ignore-fields= : The fields to be ignored}";
+    protected $signature = "seed:generate {model?} {--all} {--without-relations} {--ids= : The ids to be seeded} {--ignore-ids= : The ids to be ignored} {--fields= : The fields to be seeded} {--ignore-fields= : The fields to be ignored} {--relations= : The relations to be seeded}";
     protected $description = "Generate a seed file from a model";
+    private $oldLaravelVersion = false;
+    public function __construct__()
+    {
+        parent::__construct();
+        $this->oldLaravelVersion = version_compare(app()->version(), "8.0.0") < 0;
+    }
 
     public function handle(Filesystem $files)
     {
         try {
-            $modelPath = $this->checkModel($this->argument("model"));
-            $modelInstance = app($modelPath);
-
-            $selectedIds = $this->optionToArray($this->option("ids"));
-            $ignoreIds = $this->optionToArray($this->option("ignore-ids"));
-
-            if (count($selectedIds) > 0 && count($ignoreIds) > 0) {
-                throw new \Exception("You can't use --ignore-fields and --selected-fields at the same time.");
+            $model = $this->checkModelInput("model");
+            $modelInstance = app($model);
+            
+            if(!$this->option("all")) {
+                list($selectedIds, $ignoreIds) = $this->checkIdsInput();
+                list($selectedFields, $ignoreFields) = $this->checkFieldsInput();
+            }else {
+                $selectedIds = [];
+                $ignoreIds = [];
+                $selectedFields = [];
+                $ignoreFields = [];
             }
 
-            $selectedFields = $this->optionToArray($this->option("fields"));
-            $ignoreFields = $this->optionToArray($this->option("ignore-fields"));
+            $relations = $this->checkRelationInput();
 
-            if (count($ignoreFields) > 0 && count($selectedFields) > 0) {
-                throw new \Exception("You can't use --ignore-fields and --selected-fields at the same time.");
-            }
-
-            $seederCommands = $this->getSeederCode($modelInstance, $selectedIds, $ignoreIds, $selectedFields, $ignoreFields);
+            $seederCommands = $this->getSeederCode($modelInstance, $selectedIds, $ignoreIds, $selectedFields, $ignoreFields, $relations);
 
             $this->writeSeederFile($files, $seederCommands, $modelInstance);
         } catch (\Exception $e) {
@@ -41,23 +45,97 @@ class SeedGeneratorCommand extends Command
         }
     }
 
+    private function checkModelInput(): string
+    {
+        $model = $this->argument("model");
+        if (!$model) {
+            $model = $this->anticipate("Please provide a model name", []);
+        }
+        return $this->checkModel($model);
+    }
+
     private function checkModel(string $model): string
     {
-        //check if model name is provided
-        if (!$model) {
-            throw new \Exception("Please provide a model name using the --model option.");
-        }
-
-        //check if model file exists in \App\Models
-        $modelPath = "\\App\\Models\\{$model}";
-        if (!class_exists($modelPath)) {
-            $modelPath = "\\App\\{$model}.php";
+        if($this->oldLaravelVersion) {
+            $modelPath = "\\App\\{$model}";
             if (class_exists($modelPath)) {
                 return "\\App\\$model";
             }
             throw new \Exception("Model file not found at {$modelPath}");
+        }else{
+            $modelPath = "\\App\\Models\\{$model}";
+            if (class_exists($modelPath)) {
+                return "\\App\\Models\\$model";
+            }
+            throw new \Exception("Model file not found at {$modelPath}");
         }
-        return "\\App\\Models\\$model";
+    }
+
+    private function checkIdsInput():array
+    {
+        $selectedIds = $this->option("ids");
+        $ignoredIds = $this->option("ignore-ids");
+        if($selectedIds == null && $ignoredIds == null) {
+            $typeOfIds = $this->choice("Do you want to select or ignore ids?", [1 => "Select all", 2 => "Select some ids", 3 => "Ignore some ids"]);
+            switch($typeOfIds) {
+                case "Select some ids":
+                    $selectedIds = $this->ask("Please provide the ids you want to select (seperate with comma)");
+                    break;
+                case "Ignore some ids":
+                    $ignoredIds = $this->ask("Please provide the ids you want to ignore (seperate with comma)");
+                    break;
+            }
+        }
+        $selectedIds = $this->optionToArray($selectedIds);
+        $ignoredIds = $this->optionToArray($ignoredIds);
+        if (count($selectedIds) > 0 && count($ignoredIds) > 0) {
+            throw new \Exception("You can't use --ignore-ids and --ids at the same time.");
+        }
+        return [$selectedIds, $ignoredIds];   
+    }
+
+    private function checkFieldsInput():array
+    {
+        $selectedFields = $this->option("fields");
+        $ignoredFields = $this->option("ignore-fields");
+        if($selectedFields == null && $ignoredFields == null) {
+            $typeOfFields = $this->choice("Do you want to select or ignore fields?", [1 => "Select all", 2 => "Select some fields", 3 => "Ignore some fields"]);
+            switch($typeOfFields) {
+                case "Select some fields":
+                    $selectedFields = $this->ask("Please provide the fields you want to select (seperate with comma)");
+                    break;
+                case "Ignore some fields":
+                    $ignoredFields = $this->ask("Please provide the fields you want to ignore (seperate with comma)");
+                    break;
+            }
+        }
+        $selectedFields = $this->optionToArray($selectedFields);
+        $ignoredFields = $this->optionToArray($ignoredFields);
+        if (count($selectedFields) > 0 && count($ignoredFields) > 0) {
+            throw new \Exception("You can't use --ignore-fields and --fields at the same time.");
+        }
+        return [$selectedFields, $ignoredFields];   
+    }
+
+    private function checkRelationInput():array
+    {
+        if(!$this->option("without-relations")){
+            $relations = $this->option("relations");
+            if($relations == null) {
+                $typeOfRelation = $this->choice("Do you want to seed the has-many relation?", [1 => "No", 2 => "Yes"]);
+                switch($typeOfRelation) {
+                    case "Yes":
+                        $relations = $this->ask("Please provide the has-many relations you want to seed (seperate with comma)");
+                        break;
+                    default:
+                        $relations = "";
+                        break;
+                }
+            }
+            $relations = $this->optionToArray($relations);
+            return $relations;
+        }
+        return [];
     }
 
     private function optionToArray(?string $ids): array
@@ -74,7 +152,8 @@ class SeedGeneratorCommand extends Command
         array $selectedIds,
         array $ignoreIds,
         array $selectedFields,
-        array $ignoreFields
+        array $ignoreFields,
+        array $relations
     ): string {
         $modelInstance = $modelInstance->newQuery();
         if (count($selectedIds) > 0) {
@@ -109,6 +188,38 @@ class SeedGeneratorCommand extends Command
                 $code = StringHelper::generateIndentation($code, 2);
             }
 
+            foreach($relations as $relation) {
+                $relation = Str::camel($relation);
+                $relationData = $data->$relation;
+                //get the has many relation only
+                if($data->$relation() instanceof \Illuminate\Database\Eloquent\Relations\HasMany){
+                    if($relationData->count() > 0) {
+                        $relationSubDatas = $relationData->toArray();
+                        $relationCode = "";
+                        foreach($relationSubDatas as $subRelationKey=>$relationSubData){
+                            $relationSubData = StringHelper::prettyPrintArray($relationSubData, 4);
+                            $relationSubData = str_replace(["array (", ")"], ["[", "]"], $relationSubData);
+                            $relationSubData = rtrim($relationSubData, ",]") . "]";
+                            if($subRelationKey > 0){
+                                $relationSubData = StringHelper::generateIndentation($relationSubData, 4);
+                            }else{
+                                $relationSubData = StringHelper::generateIndentation($relationSubData, 3);
+                            }
+                            $relationCode .= ($subRelationKey>-1 ? "\n" : "").$relationSubData . ",";
+                        }
+                        // Remove trailing comma
+                        $relationCode = rtrim($relationCode, ",]") . "]";
+                        $relationCode = "\$newData$key->$relation()->createMany([" . $relationCode;
+                        $relationCode = "\n".StringHelper::generateIndentation($relationCode, 2);
+                        $relationCode .= "\n".StringHelper::generateIndentation("]);", 2);
+                        $code .= $relationCode;
+                        
+                        // $code = StringHelper::generateIndentation($code, 1);
+                    }
+                }else{
+                    throw new \Exception("The relation {$relation} is not a has-many relation");
+                }
+            }
             $codes[] = $code;
         }
         $code = implode("\n", $codes);
@@ -122,18 +233,16 @@ class SeedGeneratorCommand extends Command
     {
         $isReplace = false;
 
-        //get $modelInstance class name
-        $seedClassName = class_basename($modelInstance->getModel());
-        // Changed the suffix to 'Seeder'
+        //set seed class name
+        $seedClassName = class_basename($modelInstance);
         $seedClassName = Str::studly($seedClassName) . "Seeder";
+        //set seed namespace
+        $seedNamespace = new \ReflectionClass($modelInstance);
+        $seedNamespace = $seedNamespace->getNamespaceName();
+        $seedNamespace = str_replace("App\\Models", "", $seedNamespace);
 
-        if (version_compare(app()->version(), "8.0.0") >= 0) {
-            $seedNamespace = class_basename($modelInstance->getModel());
-            $seedNamespace = str_replace("\\$seedNamespace", "", $seedNamespace);
-            $seedNamespace = str_replace("$seedNamespace", "", $seedNamespace);
-            if ($seedNamespace != "") {
-                $seedNamespace .= "\\{$seedNamespace}";
-            }
+        if (!$this->oldLaravelVersion) {
+            
             $dirSeed = "seeders";
             $stubContent = $files->get(__DIR__ . "/../Stubs/SeedAfter8.stub");
             $fileContent = str_replace(
@@ -147,16 +256,15 @@ class SeedGeneratorCommand extends Command
             $fileContent = str_replace(["{{ class }}", "{{ code }}"], [$seedClassName, $code], $stubContent);
         }
 
+        $dirSeed .= $seedNamespace ? $seedNamespace : "";
+
         //check if seed directory exists
         if (!$files->exists(database_path($dirSeed))) {
             $files->makeDirectory(database_path($dirSeed));
         }
 
         //get $modelInstance namespace
-        $modelNamespace = get_class($modelInstance->getModel());
-        $modelNamespace = str_replace("App\Models\\", "", $modelNamespace);
-        $modelNamespace = str_replace("App\\", "", $modelNamespace);
-        $filePath = database_path("{$dirSeed}/{$modelNamespace}Seeder.php");
+        $filePath = database_path("{$dirSeed}".("\\".$seedClassName).".php");
 
         if ($files->exists($filePath)) {
             $isReplace = true;
