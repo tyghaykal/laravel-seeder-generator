6f9199ed8ca46718311c0b36401873c9dd4b8788
Andre Haykal Rachman <andrecsd9@gmail.com>
2024-02-23 23:31:57 +0700

    feat(config): debug test

--- tests/TableCommandTest.php
+++ tests/TableCommandTest.php
@@ -87,8 +87,10 @@ class TableCommandTest extends TestCase
             "--tables" => $table,
         ])->assertExitCode(0);
 
+        //dump all files under seeds folder
+        dump("ALL FILES", File::allFiles(database_path($this->folderSeeder)));
+
         // Now we should check if the file was created
-        dump("file_exist", File::exists(database_path("{$this->folderSeeder}/Tables/TestModelsSeeder.php")));
         $this->assertTrue(File::exists(database_path("{$this->folderSeeder}/Tables/TestModelsSeeder.php")));
 
         $expectedOutput = str_replace(
