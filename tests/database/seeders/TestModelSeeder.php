89c3c77d6d808ffb0b9bed6f3935b43ec40be0c0
Andre Haykal Rachman <andrecsd9@gmail.com>
2026-06-03 23:46:11 +0700

    refactor(test-seeder): clean timestamp formatting
    extract a private helper method for formatted timestamps, replace duplicate hardcoded format calls and use the TestModel's defined date format instead of static Y-m-d H:i:s

--- tests/database/seeders/TestModelSeeder.php
+++ tests/database/seeders/TestModelSeeder.php
@@ -6,6 +6,12 @@ use App\Models\TestModel;
 
 class TestModelSeeder extends Seeder
 {
+    private function getFormattedTimestamp()
+    {
+        return \Carbon\Carbon::parse("2023-05-18T11:02:55.000000Z")
+            ->format((new TestModel())->getDateFormat());
+    }
+
     /**
      * Run the database seeds.
      *
@@ -13,57 +19,59 @@ class TestModelSeeder extends Seeder
      */
     public function run()
     {
+        $timestamp = $this->getFormattedTimestamp();
+
         $data0 = TestModel::create([
             "name" => "test 1",
             "description" => "description 1",
-            "created_at" => \Carbon\Carbon::parse("2023-05-18T11:02:55.000000Z")->format("Y-m-d H:i:s"),
-            "updated_at" => \Carbon\Carbon::parse("2023-05-18T11:02:55.000000Z")->format("Y-m-d H:i:s"),
+            "created_at" => $timestamp,
+            "updated_at" => $timestamp,
         ]);
         //create data on relation test_model_childs
         $data0->test_model_childs()->createMany([
             [
                 "test_model_id" => $data0->id,
                 "name" => "child 1",
-                "created_at" => \Carbon\Carbon::parse("2023-05-18T11:02:55.000000Z")->format("Y-m-d H:i:s"),
-                "updated_at" => \Carbon\Carbon::parse("2023-05-18T11:02:55.000000Z")->format("Y-m-d H:i:s"),
+                "created_at" => $timestamp,
+                "updated_at" => $timestamp,
             ],
             [
                 "test_model_id" => $data0->id,
                 "name" => "child 2",
-                "created_at" => \Carbon\Carbon::parse("2023-05-18T11:02:55.000000Z")->format("Y-m-d H:i:s"),
-                "updated_at" => \Carbon\Carbon::parse("2023-05-18T11:02:55.000000Z")->format("Y-m-d H:i:s"),
+                "created_at" => $timestamp,
+                "updated_at" => $timestamp,
             ],
         ]);
 
         $data1 = TestModel::create([
             "name" => "test 2",
             "description" => "description 2",
-            "created_at" => \Carbon\Carbon::parse("2023-05-18T11:02:55.000000Z")->format("Y-m-d H:i:s"),
-            "updated_at" => \Carbon\Carbon::parse("2023-05-18T11:02:55.000000Z")->format("Y-m-d H:i:s"),
+            "created_at" => $timestamp,
+            "updated_at" => $timestamp,
         ]);
 
         $data1->test_model_childs()->createMany([
             [
                 "test_model_id" => $data1->id,
                 "name" => "child 2",
-                "created_at" => \Carbon\Carbon::parse("2023-05-18T11:02:55.000000Z")->format("Y-m-d H:i:s"),
-                "updated_at" => \Carbon\Carbon::parse("2023-05-18T11:02:55.000000Z")->format("Y-m-d H:i:s"),
+                "created_at" => $timestamp,
+                "updated_at" => $timestamp,
             ],
         ]);
 
         $data2 = TestModel::create([
             "name" => "test 3",
             "description" => "description 3",
-            "created_at" => \Carbon\Carbon::parse("2023-05-18T11:02:55.000000Z")->format("Y-m-d H:i:s"),
-            "updated_at" => \Carbon\Carbon::parse("2023-05-18T11:02:55.000000Z")->format("Y-m-d H:i:s"),
+            "created_at" => $timestamp,
+            "updated_at" => $timestamp,
         ]);
 
         $data2->test_model_childs()->createMany([
             [
                 "test_model_id" => $data2->id,
                 "name" => "child 3",
-                "created_at" => \Carbon\Carbon::parse("2023-05-18T11:02:55.000000Z")->format("Y-m-d H:i:s"),
-                "updated_at" => \Carbon\Carbon::parse("2023-05-18T11:02:55.000000Z")->format("Y-m-d H:i:s"),
+                "created_at" => $timestamp,
+                "updated_at" => $timestamp,
             ],
         ]);
     }
